16222e0d8c66cd1da37e419469bfcf919a1c7ec4
Hemshankar Agarwal <vasukinfc@gmail.com>
2026-09-07 17:13:42 +0530

    Add simulator gameplay loader

---
+++ hemshankar-agarwal/hackathon-game.js
@@ -0,0 +1,1 @@
+document.write('<script src="hackathon-game-1.js"><\/script><script src="hackathon-game-2.js"><\/script><script src="hackathon-game-3.js"><\/script><script src="hackathon-game-4.js"><\/script>');
